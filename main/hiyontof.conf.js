f088f36edbf9f9dc2ebfec701e0def2ab8cef3b6
Shunshun94 <shunshun9460@gmail.com>
2016-07-09 01:17:29 +0900

    Add hiyontof.conf.js

---
+++ main/hiyontof.conf.js
@@ -0,0 +1,54 @@
+/**
+ * hiyontof config file
+ * 
+ * ひよんとふの設定用 JavaScript です。
+ */
+
+/**
+ * 名前空間
+ */
+var com = com || {};
+com.hiyoko = com.hiyoko || {};
+com.hiyoko.tofclient = com.hiyoko.tofclient || {};
+
+
+/** 
+ * ログインできるサーバ一覧
+ */
+
+com.hiyoko.tofclient.ServerList = com.hiyoko.tofclient.ServerList || {};
+
+// 最初から登録されているサーバ一覧。
+// ここに追加すれば別のサーバをデフォルトで表示させておくことができるし、
+// ここから削除すればそのサーバはデフォルトでは表示されなくなる。
+// 例えば、もしも自鯖にひよんとふを設置するならば、あなたのサーバをここに追加するといいかもしれない。
+
+com.hiyoko.tofclient.ServerList.SERVER_LIST = {
+		'https://www.taruki.com/DodontoF_srv1/DodontoF.swf':'公式第三世代第壱鯖',
+		'https://www.taruki.com/DodontoF_srv2/DodontoF.swf':'公式第三世代第弐鯖',
+		'https://www.taruki.com/DodontoF_srv3/DodontoF.swf':'公式第三世代第参鯖',
+		'https://www.taruki.com/DodontoF_srv4/DodontoF.swf':'公式第三世代第四鯖',
+		'http://onse01.wtrpg.com/DodontoF/DodontoF.swf':'クラウドゲート公式',
+		'http://dodontof.incoglab.com/DodontoF.swf':'インコグ・ラボ',
+		'http://d1.trpg.net/DodontoF.swf':'TRPG.NET 第1',
+		'http://d2.trpg.net/DodontoF.swf':'TRPG.NET 第2',
+		'http://d3.trpg.net/DodontoF.swf':'TRPG.NET 第3',
+		'http://trpg.gigafreaks.com/dodontof/DodontoF.swf':'如月翔也さん',
+		'http://egotex.net/DodontoF/DodontoF.swf':'EGOTEX',
+		'https://dodontof.cokage.works/': 'こかげサーバ'
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main/js/src/tofServerList.js
+++ main/js/src/tofServerList.js
@@ -38,17 +38,6 @@ com.hiyoko.tofclient.ServerList.prototype.appendListToStorage = function(url, op
 	this.saveListToStorage(list);
 };
 
-com.hiyoko.tofclient.ServerList.SERVER_LIST = {
-		'https://www.taruki.com/DodontoF_srv1/DodontoF.swf':'公式第三世代第壱鯖',
-		'https://www.taruki.com/DodontoF_srv2/DodontoF.swf':'公式第三世代第弐鯖',
-		'https://www.taruki.com/DodontoF_srv3/DodontoF.swf':'公式第三世代第参鯖',
-		'https://www.taruki.com/DodontoF_srv4/DodontoF.swf':'公式第三世代第四鯖',
-		'http://onse01.wtrpg.com/DodontoF/DodontoF.swf':'クラウドゲート公式',
-		'http://dodontof.incoglab.com/DodontoF.swf':'インコグ・ラボ',
-		'http://d1.trpg.net/DodontoF.swf':'TRPG.NET 第1',
-		'http://d2.trpg.net/DodontoF.swf':'TRPG.NET 第2',
-		'http://d3.trpg.net/DodontoF.swf':'TRPG.NET 第3',
-		'http://trpg.gigafreaks.com/dodontof/DodontoF.swf':'如月翔也さん',
-		'http://egotex.net/DodontoF/DodontoF.swf':'EGOTEX',
-		'https://dodontof.cokage.works/': 'こかげサーバ'
-};
+// See hiyontof.conf.js
+com.hiyoko.tofclient.ServerList.SERVER_LIST = {};
+
